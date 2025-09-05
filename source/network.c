8282607ee21101487f786053ba6282e898fe2e41
Joshua Peisach <itzswirlz2020@outlook.com>
2025-09-05 13:34:14 -0400

    Move over definitions for socket, bind, and setsockopt from libogc

--- source/network.c
+++ source/network.c
@@ -2,10 +2,11 @@
 // https://github.com/devkitPro/libogc/blob/master/libogc/network_wii.c
 #include <string.h>
 
-#include "network.h"
+#include "errno.h"
 #include "heap.h"
 #include "ipc.h"
 #include "mem.h"
+#include "network.h"
 #include "syscalls.h"
 #include "types.h"
 
@@ -13,10 +14,78 @@ static char __manage_fs[] ATTRIBUTE_ALIGN(32) = "/dev/net/ncd/manage";
 static char __iptop_fs[] ATTRIBUTE_ALIGN(32) = "/dev/net/ip/top";
 static char __kd_fs[] ATTRIBUTE_ALIGN(32) = "/dev/net/kd/request";
 
-static s32 msg_queue;
 static s32 ncd_fd = -1;
 static s32 iptop_fd = -1;
 static s32 kd_fd = -1;
+
+s32 setsockopt(u32 s, u32 level, u32 optname, const void *optval, socklen_t optlen)
+{
+    s32 ret;
+    STACK_ALIGN(struct setsockopt_params, params, 1, 32);
+
+    // TODO: In release builds, remove the sanity checks since we will be doing this properly
+    if (iptop_fd < 0)
+        return -ENXIO; // no such device
+    if (optlen < 0 || optlen > 20)
+        return -EINVAL;
+
+    memset(params, 0, sizeof(struct setsockopt_params));
+    params->socket = s;
+    params->level = level;
+    params->optname = optname;
+    params->optlen = optlen;
+    if (optval && optlen)
+        memcpy(params->optval, optval, optlen);
+
+    return os_ioctl(iptop_fd, IOCTL_SO_SETSOCKOPT, params, sizeof(struct setsockopt_params), NULL,
+                    0);
+}
+
+s32 socket(u32 domain, u32 type, u32 protocol)
+{
+    s32 ret;
+    STACK_ALIGN(u32, params, 3, 32);
+
+    // TODO: In release builds, remove the sanity checks since we will be doing this properly
+    if (iptop_fd < 0)
+        return -ENXIO;
+
+    params[0] = domain;
+    params[1] = type;
+    params[2] = protocol;
+
+    ret = os_ioctl(iptop_fd, IOCTL_SO_SOCKET, params, 12, NULL, 0);
+    if (ret >= 0) {
+        // libogc: "set tcp window size to 32kb"
+        // TODO: do we need to
+        int window_size = 32768;
+        setsockopt(ret, SOL_SOCKET, SO_RCVBUF, (char *)&window_size, sizeof(window_size));
+    }
+
+    return ret;
+}
+
+s32 bind(u32 s, struct sockaddr *addr, socklen_t addrlen)
+{
+    s32 ret;
+    STACK_ALIGN(struct bind_params, params, 1, 32);
+
+    // TODO: In release builds, remove the sanity checks since we will be doing this properly
+    if (iptop_fd < 0)
+        return -ENXIO; // no such device
+
+    // We only support AF_INET, so no need to check for it:
+    // if(addr->sa_family != AF_INET) return -EAFNOSUPPORT;
+
+    addr->sa_len = sizeof(struct wii_sockaddr_in);
+
+    memset(params, 0, sizeof(struct bind_params));
+    params->socket = s;
+    params->has_name = 1;
+    memcpy(params->name, addr, sizeof(struct wii_sockaddr_in));
+
+    return os_ioctl(iptop_fd, IOCTL_SO_BIND, params, sizeof(struct bind_params), NULL, 0);
+}
 
 s32 net_init(void)
 {
--- source/network.h
+++ source/network.h
@@ -52,4 +52,91 @@ enum {
 #define IPC_ENOENT -6
 #define IPC_ENOMEM -22
 
+// From
+// https://github.com/devkitPro/libogc/blob/475da950ccf7a294e15abe985e44cedc2d6d9caa/libogc/network_wii.c#L121:
+// wii doesn't have, or use, sin_zero in sockaddr_in and all communications assumes its not there
+// bsd sockets does have it, so we ignore it. the struct is directly compatible with bsd sockets
+// normally
+struct wii_sockaddr_in {
+    u8 sin_len;
+    u8 sin_family;
+    u16 sin_port;
+    u32 sin_addr;
+};
+
+struct setsockopt_params {
+    u32 socket;
+    u32 level;
+    u32 optname;
+    u32 optlen;
+    u8 optval[20];
+};
+
+// FIXME: shouldn't `name` be renamed to `addr`?
+struct bind_params {
+    u32 socket;
+    u32 has_name;
+    u8 name[28];
+};
+
+struct sockaddr {
+    u8 sa_len;
+    u8 sa_family;
+    s8 sa_data[14];
+};
+
+/*
+ * Option flags per-socket.
+ */
+#define SO_DEBUG       0x0001 /* turn on debugging info recording */
+#define SO_ACCEPTCONN  0x0002 /* socket has had listen() */
+#define SO_REUSEADDR   0x0004 /* allow local address reuse */
+#define SO_KEEPALIVE   0x0008 /* keep connections alive */
+#define SO_DONTROUTE   0x0010 /* just use interface addresses */
+#define SO_BROADCAST   0x0020 /* permit sending of broadcast msgs */
+#define SO_USELOOPBACK 0x0040 /* bypass hardware when possible */
+#define SO_LINGER      0x0080 /* linger on close if data present */
+#define SO_OOBINLINE   0x0100 /* leave received OOB data in line */
+#define SO_REUSEPORT   0x0200 /* allow local address & port reuse */
+
+#define SO_DONTLINGER (int)(~SO_LINGER)
+
+/*
+ * Additional options, not kept in so_options.
+ */
+#define SO_SNDBUF   0x1001 /* send buffer size */
+#define SO_RCVBUF   0x1002 /* receive buffer size */
+#define SO_SNDLOWAT 0x1003 /* send low-water mark */
+#define SO_RCVLOWAT 0x1004 /* receive low-water mark */
+#define SO_SNDTIMEO 0x1005 /* send timeout */
+#define SO_RCVTIMEO 0x1006 /* receive timeout */
+#define SO_ERROR    0x1007 /* get error status and clear */
+#define SO_TYPE     0x1008 /* get socket type */
+
+/*
+ * Level number for (get/set)sockopt() to apply to socket itself.
+ */
+#define SOL_SOCKET 0xfff /* options for socket level */
+
+#define AF_UNSPEC 0
+#define AF_INET   2
+#define PF_INET   AF_INET
+#define PF_UNSPEC AF_UNSPEC
+
+#define IPPROTO_IP  0
+#define IPPROTO_TCP 6
+#define IPPROTO_UDP 17
+
+#define INADDR_ANY       0
+#define INADDR_BROADCAST 0xffffffff
+
+/* Flags we can use with send and recv. */
+#define MSG_DONTWAIT 0x40 /* Nonblocking i/o for this operation only */
+
+#define socklen_t int
+
+s32 socket(u32 domain, u32 type, u32 protocol);
+s32 bind(u32 s, struct sockaddr *addr, socklen_t addrlen);
+s32 setsockopt(u32 s, u32 level, u32 optname, const void *optval, socklen_t optlen);
+
 s32 net_init(void);
